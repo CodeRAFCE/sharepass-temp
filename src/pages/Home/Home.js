359ace60adc6c7c4ec9f3ad79a85297406bf749f
CodeRAFCE <seshan.io@prismscale.com>
2023-08-16 17:00:32 +0530

    compressed versions of  the videos

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -126,16 +126,15 @@ function Home() {
 			{false && <Statistics />}
 			<OurCustomers />
 
+			{/* -------------------------------------------- */}
 			<MobilePrivacy title1={page.sec_3_title} para1={page.sec_3_desc} image1={page.sec_3_image} />
-
 			<FeatureV2 title1={page.sec_2_title} para1={page.sec_2_desc} image1={page.sec_2_image} />
-
 			<EnterpriseVideoSection
 				title1={page.sec_4_title}
 				para1={page.sec_4_desc}
 				image1={page.sec_4_image}
 			/>
-
+			{/* -------------------------------------------- */}
 			{/* <Features
         title1={page.sec_2_title}
         title2={page.sec_3_title}
