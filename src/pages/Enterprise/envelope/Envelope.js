7beba6b71bd95efcf564f629821190fb9be14feb
CodeRAFCE <seshan.io@prismscale.com>
2023-09-11 16:25:34 +0530

    changes in the video files

--- src/pages/Enterprise/envelope/Envelope.js
+++ src/pages/Enterprise/envelope/Envelope.js
@@ -1,72 +1,94 @@
-import './envelope.css';
-import http1 from '../../variables';
-import { Fragment, useRef, useEffect, useState } from 'react';
-import { InView } from 'react-intersection-observer';
+import "./envelope.css";
+import http1 from "../../variables";
+import {Fragment, useRef, useEffect, useState} from "react";
+import {InView} from "react-intersection-observer";
 import envelopeVideo from "../../../assets/animations/Envelope.mp4";
 import envelopeVideoVer from "../../../assets/animations/Envelope-ver.mp4";
+
+// webm
+import envelopeVideoDesktop from "../../../assets/webm/envelope-desktop.webm";
+import envelopeVideoMobile from "../../../assets/webm/envelope-mobile.webm";
+
 import {Player} from "@lottiefiles/react-lottie-player";
 
 // lottie files
 //import envelopeVideo from "../../../assets/lottie/privacy-solution.json";
 
 const Envelope = (props) => {
+	const [first, setFirst] = useState(false);
 
-    const [first, setFirst] = useState(false);
-
-
-    return (
-        <Fragment>
-            {false && <section className='features_sec' id='features'>
-                <div className='content_sec'>
-                    <InView onChange={(inView, entry) => {
-                        if (inView) {
-                            setFirst(true)
-                        } else {
-                            setFirst(false)
-                        }
-                    }}>
-                        <div className='flap center' style={{opacity:first ? 1 : 0, transition:'all 1.5s ease-in-out'}}>
-                            
-                            <div className='video_wrap'>
-                                <video
-                                    className='img_video'
-                                    src={envelopeVideo}
-                                    autoPlay
-                                    loop
-                                    muted
-                                    playsInline
-                                />
-                                {/*<Player src={envelopeVideo} loop autoplay />*/}
-                            </div>
-                            <div className='text_wrap pl'>
-                                <h2>Receive data securely</h2>
-                                <p>Use our envelope feature to request confidential data from third parties securely and smoothly</p>
-                            </div>
-                        </div>
-                    </InView>
-                </div>
-            </section>}
-            <InView onChange={(inView, entry) => {
-                if (inView) {
-                    setFirst(true)
-                } else {
-                    setFirst(false)
-                }
-            }}>
-                <section className="section-container">
-                    
-                        <div className="content-wrapper content-wrapper-center-end">
-                                <div className="content-wrap pad-left">
-                                    <h2>Receive data securely</h2>
-                                    <p>Use our envelope feature to request confidential data from third parties securely and smoothly</p>
-                                </div>
-                                <video className="desktop-video" src={envelopeVideo} autoPlay loop playsInline muted></video>
-                                <video className="mobile-video" src={envelopeVideoVer} autoPlay loop playsInline muted></video>
-                        </div>
-                </section>
-            </InView>
-        </Fragment>
-    )
-}
+	return (
+		<Fragment>
+			{false && (
+				<section className="features_sec" id="features">
+					<div className="content_sec">
+						<InView
+							onChange={(inView, entry) => {
+								if (inView) {
+									setFirst(true);
+								} else {
+									setFirst(false);
+								}
+							}}
+						>
+							<div
+								className="flap center"
+								style={{opacity: first ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+							>
+								<div className="video_wrap">
+									<video
+										className="img_video"
+										src={envelopeVideo}
+										autoPlay
+										loop
+										muted
+										playsInline
+									/>
+									{/*<Player src={envelopeVideo} loop autoplay />*/}
+								</div>
+								<div className="text_wrap pl">
+									<h2>Receive data securely</h2>
+									<p>
+										Use our envelope feature to request confidential data from third parties
+										securely and smoothly
+									</p>
+								</div>
+							</div>
+						</InView>
+					</div>
+				</section>
+			)}
+			<InView
+				onChange={(inView, entry) => {
+					if (inView) {
+						setFirst(true);
+					} else {
+						setFirst(false);
+					}
+				}}
+			>
+				<section className="section-container">
+					<div className="content-wrapper content-wrapper-center-end">
+						<div className="content-wrap pad-left">
+							<h2>Receive data securely</h2>
+							<p>
+								Use our envelope feature to request confidential data from third parties securely
+								and smoothly
+							</p>
+						</div>
+						<video className="desktop-video" autoPlay loop playsInline muted>
+							<source src={envelopeVideo} type="video/mp4" />
+							<source src={envelopeVideoDesktop} type="video/webm" />
+						</video>
+						<video className="mobile-video" autoPlay loop playsInline muted>
+							<source src={envelopeVideoVer} type="video/mp4" />
+							<source src={envelopeVideoMobile} type="video/webm" />
+						</video>
+					</div>
+				</section>
+			</InView>
+		</Fragment>
+	);
+};
 
 export default Envelope;
--- src/pages/Home/features/Features.js
+++ src/pages/Home/features/Features.js
@@ -11,6 +11,10 @@ import timeControlledVer from "../../../assets/animations/Time-Controlled-Secret
 import mobilePrivacy from "../../../assets/animations/Mobile-Privacy.mp4";
 import mobilePrivacyVer from "../../../assets/animations/Mobile-Privacy-ver.mp4";
 
+// webmS
+import mobilePrivacyDesktop from "../../../assets/webm/mobile-privacy-desktop.webm";
+import mobilePrivacyMobile from "../../../assets/webm/mobile-privacy-mobile.webm";
+
 import Envelope from "../../../pages/Enterprise/envelope/Envelope";
 // lottie file
 // import timeControlled from "../../../assets/lottie/time-controlled.json";
@@ -27,110 +31,66 @@ const Features = (props) => {
 				<div className="content-wrapper content-wrapper-center-start">
 					<div className="content-wrap pad-left">
 						<h2>Time-Controlled Secrets</h2>
-						<p>Empower users with personalized access to sensitive data that offer scheduled releases, time-limited availability, and single or multiple access options for a superior user experience.</p>
+						<p>
+							Empower users with personalized access to sensitive data that offer scheduled
+							releases, time-limited availability, and single or multiple access options for a
+							superior user experience.
+						</p>
 					</div>
-					<video className="desktop-video" src={timeControlled} autoPlay loop playsInline muted></video>
-					<video className="mobile-video" src={timeControlledVer} autoPlay loop playsInline muted></video>
+					<video className="desktop-video" autoPlay loop playsInline muted>
+						<source src={timeControlled} type="video/mp4" />
+						{/* <source src={timeControlled} type="video/webm" /> */}
+					</video>
+					<video className="mobile-video" autoPlay loop playsInline muted>
+						<source src={timeControlledVer} type="video/mp4" />
+						{/* <source src={timeControlledVer} type="video/webm" /> */}
+					</video>
 				</div>
 			</section>
+
 			<section className="section-container">
 				<div className="content-wrapper content-wrapper-center-end">
 					<div className="dark-content pad-left">
 						<h2>Your Mobile Privacy solution</h2>
-						<p>The AI-powered SharePass Keyboard secures your data on any app, eliminating digital footprints. Protect your privacy on any mobile app.</p>
+						<p>
+							The AI-powered SharePass Keyboard secures your data on any app, eliminating digital
+							footprints. Protect your privacy on any mobile app.
+						</p>
 					</div>
-					<video className="desktop-video" src={mobilePrivacy} autoPlay loop playsInline muted></video>
-					<video className="mobile-video" src={mobilePrivacyVer} autoPlay loop playsInline muted></video>
+					<video className="desktop-video" autoPlay loop playsInline muted>
+						<source src={mobilePrivacy} type="video/mp4" />
+						<source src={mobilePrivacyDesktop} type="video/webm" />
+					</video>
+					<video className="mobile-video" autoPlay loop playsInline muted>
+						<source src={mobilePrivacyVer} type="video/mp4" />
+						<source src={mobilePrivacyMobile} type="video/webm" />
+					</video>
 				</div>
 			</section>
-			{false && <section className="features_sec" id="features">
-				<div className="content_sec">
-					<InView
-						onChange={(inView, entry) => {
-							if (inView) {
-								setFirst(true);
-							} else {
-								setFirst(false);
-							}
-						}}
-					>
-						<div
-							className="flap"
-							style={{opacity: first ? 1 : 0, transition: "all 1.5s ease-in-out"}}
-						>
-							<div className="video_wrap">
-								{/* <Lottie animationData={timeControlled} loop play /> */}
-								{/* <Player src={timeControlled} loop autoplay /> */}
-								<video
-									className="img_video"
-									//src={`${http1}${props.image2}`}
-									src={timeControlled}
-									autoPlay
-									loop
-									muted
-									playsInline
-								/>
-							</div>
-							<div className="text_wrap pl">
-								<h2>{props.title1}</h2>
-								<p>{props.para1}</p>
-							</div>
-						</div>
-					</InView>
 
-					<InView
-						onChange={(inView, entry) => {
-							if (inView) {
-								setSecond(true);
-							} else {
-								setSecond(false);
-							}
-						}}
-					>
-						<div
-							className="flap center"
-							style={{opacity: second ? 1 : 0, transition: "all 1.5s ease-in-out"}}
-						>
-							<div className="text_wrap pr">
-								<h2>{props.title2}</h2>
-								<p>{props.para2}</p>
-							</div>
-
-							<div className="video_wrap">
-								{/*<Player src={mobilePrivacy} loop autoplay />*/}
-								{
-									<video
-										className="img_video"
-										//src={`${http1}${props.image2}`}
-										src={mobilePrivacy}
-										autoPlay
-										loop
-										muted
-										playsInline
-									/>
-								}
-							</div>
-						</div>
-					</InView>
-
-					{false && (
+			{false && (
+				<section className="features_sec" id="features">
+					<div className="content_sec">
 						<InView
 							onChange={(inView, entry) => {
 								if (inView) {
-									setThird(true);
+									setFirst(true);
 								} else {
-									setThird(false);
+									setFirst(false);
 								}
 							}}
 						>
 							<div
 								className="flap"
-								style={{opacity: third ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+								style={{opacity: first ? 1 : 0, transition: "all 1.5s ease-in-out"}}
 							>
 								<div className="video_wrap">
+									{/* <Lottie animationData={timeControlled} loop play /> */}
+									{/* <Player src={timeControlled} loop autoplay /> */}
 									<video
 										className="img_video"
-										src={`${http1}${props.image3}`}
+										//src={`${http1}${props.image2}`}
+										src={timeControlled}
 										autoPlay
 										loop
 										muted
@@ -138,14 +98,81 @@ const Features = (props) => {
 									/>
 								</div>
 								<div className="text_wrap pl">
-									<h2>{props.title3}</h2>
-									<p>{props.para3}</p>
+									<h2>{props.title1}</h2>
+									<p>{props.para1}</p>
 								</div>
 							</div>
 						</InView>
-					)}
-				</div>
-			</section>}
+
+						<InView
+							onChange={(inView, entry) => {
+								if (inView) {
+									setSecond(true);
+								} else {
+									setSecond(false);
+								}
+							}}
+						>
+							<div
+								className="flap center"
+								style={{opacity: second ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+							>
+								<div className="text_wrap pr">
+									<h2>{props.title2}</h2>
+									<p>{props.para2}</p>
+								</div>
+
+								<div className="video_wrap">
+									{/*<Player src={mobilePrivacy} loop autoplay />*/}
+									{
+										<video
+											className="img_video"
+											//src={`${http1}${props.image2}`}
+											src={mobilePrivacy}
+											autoPlay
+											loop
+											muted
+											playsInline
+										/>
+									}
+								</div>
+							</div>
+						</InView>
+
+						{false && (
+							<InView
+								onChange={(inView, entry) => {
+									if (inView) {
+										setThird(true);
+									} else {
+										setThird(false);
+									}
+								}}
+							>
+								<div
+									className="flap"
+									style={{opacity: third ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+								>
+									<div className="video_wrap">
+										<video
+											className="img_video"
+											src={`${http1}${props.image3}`}
+											autoPlay
+											loop
+											muted
+											playsInline
+										/>
+									</div>
+									<div className="text_wrap pl">
+										<h2>{props.title3}</h2>
+										<p>{props.para3}</p>
+									</div>
+								</div>
+							</InView>
+						)}
+					</div>
+				</section>
+			)}
 
 			<Envelope />
 		</Fragment>
