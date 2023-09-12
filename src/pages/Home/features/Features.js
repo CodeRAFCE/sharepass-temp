cf85086018539ceac5f0b6313f58e101e75661fa
CodeRAFCE <seshan.io@prismscale.com>
2023-09-12 11:05:47 +0530

    getting it to work in firefox

--- src/pages/Home/features/Features.js
+++ src/pages/Home/features/Features.js
@@ -1,19 +1,22 @@
 import "./features.css";
 import http1 from "../../variables";
+// import videojs from "video.js";
+import "video.js/dist/video-js.css"; // Import the CSS for video.js
 import {Fragment, useRef, useEffect, useState} from "react";
 import {InView} from "react-intersection-observer";
 // import Lottie from "react-lottie-player";
 import {Player} from "@lottiefiles/react-lottie-player";
 
-// mp4s
-import timeControlled from "../../../assets/animations/Time-Controlled-Secrets.mp4";
-import timeControlledVer from "../../../assets/animations/Time-Controlled-Secrets-ver.mp4";
-import mobilePrivacy from "../../../assets/animations/Mobile-Privacy.mp4";
-import mobilePrivacyVer from "../../../assets/animations/Mobile-Privacy-ver.mp4";
-
-// webmS
-import mobilePrivacyDesktop from "../../../assets/webm/mobile-privacy-desktop.webm";
-import mobilePrivacyMobile from "../../../assets/webm/mobile-privacy-mobile.webm";
+// Import your videos
+import timeControlledDesktop from "../../../assets/animations/Time-Controlled-Secrets.mp4";
+import timeControlledMobile from "../../../assets/animations/Time-Controlled-Secrets-ver.mp4";
+import mobilePrivacyDesktop from "../../../assets/animations/Mobile-Privacy.mp4";
+import mobilePrivacyMobile from "../../../assets/animations/Mobile-Privacy-ver.mp4";
+// Import webm versions if available
+import timeControlledDesktopWebm from "../../../assets/animations/Time-Controlled-Secrets.webm";
+// import timeControlledMobileWebm from "../../../assets/animations/Time-Controlled-Secrets-ver.mp4";
+import mobilePrivacyDesktopWebm from "../../../assets/webm/mobile-privacy-desktop.webm";
+import mobilePrivacyMobileWebm from "../../../assets/webm/mobile-privacy-mobile.webm";
 
 // fallback images
 import timeControlledDesktopImage from "../../../assets/fallback_images/time-controlled-desktop.png";
@@ -31,19 +34,11 @@ const Features = (props) => {
 	const [second, setSecond] = useState(false);
 	const [third, setThird] = useState(false);
 
-	const timeControlledRef = useRef();
-	const mobilePrivacyRef = useRef();
+	const timeControlledDesktopRef = useRef();
+	const timeControlledMobileRef = useRef();
 
-	const playVideo = (videoRef) => {
-		if (videoRef.current) {
-			videoRef.current.play();
-		}
-	};
-
-	useEffect(() => {
-		playVideo(timeControlledRef);
-		playVideo(mobilePrivacyRef);
-	}, []);
+	const mobilePrivacyDesktopRef = useRef();
+	const mobilePrivacyMobileRef = useRef();
 
 	return (
 		<Fragment>
@@ -57,21 +52,40 @@ const Features = (props) => {
 							superior user experience.
 						</p>
 					</div>
-					<video className="desktop-video" ref={timeControlledRef} autoPlay loop playsInline muted>
-						<source src={timeControlled} type="video/mp4" />
-						{/* <source src={timeControlled} type="video/webm" /> */}
+					<video
+						className="desktop-video"
+						ref={timeControlledDesktopRef}
+						poster={timeControlledDesktopImage}
+						autoPlay
+						loop
+						playsInline
+						muted
+					>
+						<source src={timeControlledDesktop} type="video/mp4" />
+						<source src={timeControlledDesktopWebm} type="video/webm" />
 						<img
 							src={timeControlledDesktopImage}
 							alt="mobile privacy desktop"
+							className="desktop-video"
 							style={{width: "100%", height: "100%", objectFit: "cover"}}
 						/>
 					</video>
-					<video className="mobile-video" ref={mobilePrivacyRef} autoPlay loop playsInline muted>
-						<source src={timeControlledVer} type="video/mp4" />
-						{/* <source src={timeControlledVer} type="video/webm" /> */}
+
+					<video
+						className="mobile-video"
+						ref={timeControlledMobileRef}
+						poster={timeControlledMobileImage}
+						autoPlay
+						loop
+						playsInline
+						muted
+					>
+						<source src={timeControlledMobile} type="video/mp4" />
+						{/* <source src={timeControlledMobileWebm} type="video/webm" /> */}
 						<img
 							src={timeControlledMobileImage}
 							alt="mobile privacy desktop"
+							className="desktop-video"
 							style={{width: "100%", height: "100%", objectFit: "cover"}}
 						/>
 					</video>
@@ -87,18 +101,34 @@ const Features = (props) => {
 							footprints. Protect your privacy on any mobile app.
 						</p>
 					</div>
-					<video className="desktop-video" autoPlay loop playsInline muted>
-						<source src={mobilePrivacy} type="video/mp4" />
-						<source src={mobilePrivacyDesktop} type="video/webm" />
+
+					<video
+						className="desktop-video"
+						ref={mobilePrivacyDesktopRef}
+						autoPlay
+						loop
+						playsInline
+						muted
+					>
+						<source src={mobilePrivacyDesktop} type="video/mp4" />
+						<source src={mobilePrivacyDesktopWebm} type="video/webm" />
 						<img
 							src={mobilePrivacyDesktopImage}
 							alt="mobile privacy desktop"
 							style={{width: "100%", height: "100%", objectFit: "cover"}}
 						/>
 					</video>
-					<video className="mobile-video" autoPlay loop playsInline muted>
-						<source src={mobilePrivacyVer} type="video/mp4" />
-						<source src={mobilePrivacyMobile} type="video/webm" />
+
+					<video
+						className="mobile-video"
+						ref={mobilePrivacyMobileRef}
+						autoPlay
+						loop
+						playsInline
+						muted
+					>
+						<source src={mobilePrivacyMobile} type="video/mp4" />
+						<source src={mobilePrivacyMobileWebm} type="video/webm" />
 						<img
 							src={mobilePrivacyMobileImage}
 							alt="mobile privacy mobile"
@@ -108,115 +138,117 @@ const Features = (props) => {
 				</div>
 			</section>
 
-			{false && (
-				<section className="features_sec" id="features">
-					<div className="content_sec">
-						<InView
-							onChange={(inView, entry) => {
-								if (inView) {
-									setFirst(true);
-								} else {
-									setFirst(false);
-								}
-							}}
-						>
-							<div
-								className="flap"
-								style={{opacity: first ? 1 : 0, transition: "all 1.5s ease-in-out"}}
-							>
-								<div className="video_wrap">
-									{/* <Lottie animationData={timeControlled} loop play /> */}
-									{/* <Player src={timeControlled} loop autoplay /> */}
-									<video
-										className="img_video"
-										//src={`${http1}${props.image2}`}
-										src={timeControlled}
-										autoPlay
-										loop
-										muted
-										playsInline
-									/>
-								</div>
-								<div className="text_wrap pl">
-									<h2>{props.title1}</h2>
-									<p>{props.para1}</p>
-								</div>
-							</div>
-						</InView>
-
-						<InView
-							onChange={(inView, entry) => {
-								if (inView) {
-									setSecond(true);
-								} else {
-									setSecond(false);
-								}
-							}}
-						>
-							<div
-								className="flap center"
-								style={{opacity: second ? 1 : 0, transition: "all 1.5s ease-in-out"}}
-							>
-								<div className="text_wrap pr">
-									<h2>{props.title2}</h2>
-									<p>{props.para2}</p>
-								</div>
-
-								<div className="video_wrap">
-									{/*<Player src={mobilePrivacy} loop autoplay />*/}
-									{
-										<video
-											className="img_video"
-											//src={`${http1}${props.image2}`}
-											src={mobilePrivacy}
-											autoPlay
-											loop
-											muted
-											playsInline
-										/>
-									}
-								</div>
-							</div>
-						</InView>
-
-						{false && (
-							<InView
-								onChange={(inView, entry) => {
-									if (inView) {
-										setThird(true);
-									} else {
-										setThird(false);
-									}
-								}}
-							>
-								<div
-									className="flap"
-									style={{opacity: third ? 1 : 0, transition: "all 1.5s ease-in-out"}}
-								>
-									<div className="video_wrap">
-										<video
-											className="img_video"
-											src={`${http1}${props.image3}`}
-											autoPlay
-											loop
-											muted
-											playsInline
-										/>
-									</div>
-									<div className="text_wrap pl">
-										<h2>{props.title3}</h2>
-										<p>{props.para3}</p>
-									</div>
-								</div>
-							</InView>
-						)}
-					</div>
-				</section>
-			)}
-
 			<Envelope />
 		</Fragment>
 	);
 };
 
 export default Features;
+
+// {
+// 	false && (
+// 		<section className="features_sec" id="features">
+// 			<div className="content_sec">
+// 				<InView
+// 					onChange={(inView, entry) => {
+// 						if (inView) {
+// 							setFirst(true);
+// 						} else {
+// 							setFirst(false);
+// 						}
+// 					}}
+// 				>
+// 					<div
+// 						className="flap"
+// 						style={{opacity: first ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+// 					>
+// 						<div className="video_wrap">
+// 							{/* <Lottie animationData={timeControlled} loop play /> */}
+// 							{/* <Player src={timeControlled} loop autoplay /> */}
+// 							<video
+// 								className="img_video"
+// 								//src={`${http1}${props.image2}`}
+// 								src={timeControlled}
+// 								autoPlay
+// 								loop
+// 								muted
+// 								playsInline
+// 							/>
+// 						</div>
+// 						<div className="text_wrap pl">
+// 							<h2>{props.title1}</h2>
+// 							<p>{props.para1}</p>
+// 						</div>
+// 					</div>
+// 				</InView>
+
+// 				<InView
+// 					onChange={(inView, entry) => {
+// 						if (inView) {
+// 							setSecond(true);
+// 						} else {
+// 							setSecond(false);
+// 						}
+// 					}}
+// 				>
+// 					<div
+// 						className="flap center"
+// 						style={{opacity: second ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+// 					>
+// 						<div className="text_wrap pr">
+// 							<h2>{props.title2}</h2>
+// 							<p>{props.para2}</p>
+// 						</div>
+
+// 						<div className="video_wrap">
+// 							{/*<Player src={mobilePrivacy} loop autoplay />*/}
+// 							{
+// 								<video
+// 									className="img_video"
+// 									//src={`${http1}${props.image2}`}
+// 									src={mobilePrivacy}
+// 									autoPlay
+// 									loop
+// 									muted
+// 									playsInline
+// 								/>
+// 							}
+// 						</div>
+// 					</div>
+// 				</InView>
+
+// 				{false && (
+// 					<InView
+// 						onChange={(inView, entry) => {
+// 							if (inView) {
+// 								setThird(true);
+// 							} else {
+// 								setThird(false);
+// 							}
+// 						}}
+// 					>
+// 						<div
+// 							className="flap"
+// 							style={{opacity: third ? 1 : 0, transition: "all 1.5s ease-in-out"}}
+// 						>
+// 							<div className="video_wrap">
+// 								<video
+// 									className="img_video"
+// 									src={`${http1}${props.image3}`}
+// 									autoPlay
+// 									loop
+// 									muted
+// 									playsInline
+// 								/>
+// 							</div>
+// 							<div className="text_wrap pl">
+// 								<h2>{props.title3}</h2>
+// 								<p>{props.para3}</p>
+// 							</div>
+// 						</div>
+// 					</InView>
+// 				)}
+// 			</div>
+// 		</section>
+// 	);
+// }
